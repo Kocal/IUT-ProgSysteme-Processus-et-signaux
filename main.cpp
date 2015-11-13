04414ffea560fa6103704813382144711976c0f9
ALLIAUME Hugo <kocal@live.fr>
2015-11-13 16:26:27 +0100

    Partie 4 : kill() et pause()
    Ecrire un programme dans lequel :
    1. Un processus père crée un processus fils
    2. Le processus fils temporise avec sleep(), puis envoie le signal
    SIGUSR1 à son père
    3. Le processus père se met en attente jusqu'à la réception d'un signal
    du processus fils
    4. Le processus fils doit se terminer avant le processus père

--- main.cpp
+++ main.cpp
@@ -5,36 +5,59 @@
 #include <string>
 
 #include <signal.h>
-#include <sys/types.h>
+#include <errno.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 using std::cout;
 using std::endl;
 using std::string;
 
 void _sigusr1(int sig);
-void _sigusr2(int sig);
 
 int main(void) {
-    signal(SIGUSR1, _sigusr1);
-    signal(SIGUSR2, _sigusr2);
 
-    for(;;) {
-        cout << "?" << endl;
-        sleep(1);
+    int status;
+    pid_t pid;
+
+    signal(SIGUSR1, _sigusr1);
+
+    pid = fork();
+
+    switch(pid) {
+    case -1: {
+            perror("fork");
+            break;
+        }
+
+    case 0: {
+            cout << "Enfant créé (" << getpid() << ")" << endl;
+
+            cout << "Attente de 3 secondes..." << endl;
+            sleep(3);
+
+            cout << "Envoie du signal SIGUSR1 au père (" << getppid() << ")" << endl;
+            kill(getppid(), SIGUSR1);
+            exit(EXIT_SUCCESS);
+            break;
+        }
+
+    default: {
+            cout << "Parent (" << getpid() << ")" << endl;
+            pause();
+
+            do {
+                cout << "Attente de la fin du processus fils... " << endl;
+                waitpid(pid, &status, WUNTRACED);
+            } while(!WIFEXITED(status) && !WIFSIGNALED(status));
+        }
     }
 
+    cout << "Programme terminé !" << endl;
     return EXIT_SUCCESS;
 }
 
 void _sigusr1(int sig) {
-    cout << "Bonjour" << endl;
-    signal(SIGUSR1, SIG_IGN);
+    cout << "Le père a reçu le signal SIGUSR1" << endl;
 }
-
-void _sigusr2(int sig) {
-    cout << "Bonsoir" << endl;
-}
-
-
-
