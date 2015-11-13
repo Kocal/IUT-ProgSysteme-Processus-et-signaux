6096926c06c183d4dba5967a379619156e704e99
ALLIAUME Hugo <kocal@live.fr>
2015-11-13 15:05:36 +0100

    Ajout de la gestion des signaux SIGUSR1 et SIGUSR2
    Partie 3 du TP, on ne doit afficher qu'une seule fois "Bonjour" lorsque
    le programme reçoit le signal "SIGUSR1" (on ignore le signal au deuxième
    appel).
    
    Il doit aussi afficher "Bonsoir" lorsqu'on reçoit le signal "SIGUSR2",
    et ce, peu importe le nombre de fois d'envoi du signal.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
+#include <cstdlib>
+#include <cstdio>
 
-using namespace std;
+#include <string>
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+void _sigusr1(int sig);
+void _sigusr2(int sig);
+
+int main(void) {
+    signal(SIGUSR1, _sigusr1);
+    signal(SIGUSR2, _sigusr2);
+
+    for(;;) {
+        cout << "?" << endl;
+        sleep(1);
+    }
+
+    return EXIT_SUCCESS;
 }
+
+void _sigusr1(int sig) {
+    cout << "Bonjour" << endl;
+    signal(SIGUSR1, SIG_IGN);
+}
+
+void _sigusr2(int sig) {
+    cout << "Bonsoir" << endl;
+}
+
+
 
